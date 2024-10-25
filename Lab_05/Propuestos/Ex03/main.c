4bd4dbaa117361026e6a0783f2e009ab3df04308
Joel <jchinop@unsa.edu.pe>
2024-10-25 15:09:55 -0500

    ejercicio 04

--- Lab_05/Propuestos/Ex03/main.c
+++ Lab_05/Propuestos/Ex03/main.c
@@ -1,140 +1,120 @@
-#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <sys/shm.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
+#include <errno.h>
 
-#define CLAVE_MSG 2002
-#define CLAVE_SHM 2003
-#define OK 0
-#define ERROR -1
+#define CLAVE_MSG 1234
+#define CLAVE_MEM 5678
 
+// Estructura del mensaje
 typedef struct
 {
     long tipo;
-    int fila;
-    int columna;
+    int posicion;
 } MENSAJE;
 
-typedef struct
+// Función para imprimir la matriz del juego
+void imprimirMatriz(char *matriz)
 {
-    char tablero[3][3];
-    int turno; // 1 para X, 2 para O
-} TABLERO;
-
-void imprimir_tablero(TABLERO *t)
-{
-    printf("\n");
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 9; i++)
     {
-        for (int j = 0; j < 3; j++)
-        {
-            printf(" %c |", t->tablero[i][j]);
-        }
-        printf("\n-----------\n");
+        printf("%c ", matriz[i]);
+        if ((i + 1) % 3 == 0)
+            printf("\n");
     }
 }
 
-int verificar_ganador(TABLERO *t, char jugador)
+// Función principal del proceso de juego
+void jugar(int tipo, int msgid, char *matriz)
 {
-    for (int i = 0; i < 3; i++)
+    MENSAJE mensaje;
+    int posicion;
+    char simbolo = (tipo == 1) ? 'X' : 'O';
+
+    while (1)
     {
-        if ((t->tablero[i][0] == jugador && t->tablero[i][1] == jugador && t->tablero[i][2] == jugador) ||
-            (t->tablero[0][i] == jugador && t->tablero[1][i] == jugador && t->tablero[2][i] == jugador))
+        if (tipo == 1)
+        { // Jugador X
+            printf("Jugador X, ingrese posición (0-8): ");
+            scanf("%d", &posicion);
+            mensaje.tipo = 2; // Enviar al proceso O
+        }
+        else
+        { // Jugador O
+            printf("Esperando a Jugador X...\n");
+            mensaje.tipo = 1; // Enviar al proceso X
+        }
+
+        mensaje.posicion = posicion;
+        matriz[posicion] = simbolo; // Actualiza matriz compartida
+
+        // Enviar mensaje a otro proceso
+        if (msgsnd(msgid, &mensaje, sizeof(int), 0) == -1)
         {
-            return 1;
+            perror("msgsnd");
+            exit(EXIT_FAILURE);
+        }
+
+        imprimirMatriz(matriz); // Muestra matriz actualizada
+
+        // Esperar mensaje del otro proceso
+        if (msgrcv(msgid, &mensaje, sizeof(int), tipo, 0) == -1)
+        {
+            perror("msgrcv");
+            exit(EXIT_FAILURE);
         }
     }
-    if ((t->tablero[0][0] == jugador && t->tablero[1][1] == jugador && t->tablero[2][2] == jugador) ||
-        (t->tablero[0][2] == jugador && t->tablero[1][1] == jugador && t->tablero[2][0] == jugador))
-    {
-        return 1;
-    }
-    return 0;
 }
 
 int main()
 {
-    int qid, shmid;
-    MENSAJE msg;
-    TABLERO *tablero;
-    pid_t pid;
+    int msgid, shmid;
+    char *matriz;
 
-    qid = msgget(CLAVE_MSG, IPC_CREAT | 0666);
-    shmid = shmget(CLAVE_SHM, sizeof(TABLERO), IPC_CREAT | 0666);
-    tablero = (TABLERO *)shmat(shmid, NULL, 0);
-
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-            tablero->tablero[i][j] = ' ';
-    tablero->turno = 1;
-
-    pid = fork();
-
-    if (pid == 0)
+    // Crear cola de mensajes
+    msgid = msgget(CLAVE_MSG, IPC_CREAT | 0666);
+    if (msgid == -1)
     {
-        while (1)
-        {
-            if (tablero->turno == 2)
-            {
-                imprimir_tablero(tablero);
-                printf("Jugador O - Ingrese fila (0-2) y columna (0-2): ");
-                scanf("%d %d", &msg.fila, &msg.columna);
-                if (tablero->tablero[msg.fila][msg.columna] == ' ')
-                {
-                    tablero->tablero[msg.fila][msg.columna] = 'O';
-                    tablero->turno = 1;
-                    if (verificar_ganador(tablero, 'O'))
-                    {
-                        imprimir_tablero(tablero);
-                        printf("¡Jugador O ha ganado!\n");
-                        break;
-                    }
-                    msg.tipo = 1;
-                    msgsnd(qid, &msg, sizeof(msg) - sizeof(long), 0);
-                }
-            }
-            else
-            {
-                msgrcv(qid, &msg, sizeof(msg) - sizeof(long), 2, 0);
-            }
-        }
+        perror("msgget");
+        exit(EXIT_FAILURE);
+    }
+
+    // Crear memoria compartida
+    shmid = shmget(CLAVE_MEM, 9 * sizeof(char), IPC_CREAT | 0666);
+    if (shmid == -1)
+    {
+        perror("shmget");
+        exit(EXIT_FAILURE);
+    }
+
+    matriz = (char *)shmat(shmid, NULL, 0);
+    if (matriz == (char *)-1)
+    {
+        perror("shmat");
+        exit(EXIT_FAILURE);
+    }
+
+    // Inicializar matriz con espacios en blanco
+    for (int i = 0; i < 9; i++)
+        matriz[i] = ' ';
+
+    // Crear procesos para los jugadores
+    if (fork() == 0)
+    {
+        jugar(1, msgid, matriz); // Proceso para jugador X
     }
     else
     {
-        while (1)
-        {
-            if (tablero->turno == 1)
-            {
-                imprimir_tablero(tablero);
-                printf("Jugador X - Ingrese fila (0-2) y columna (0-2): ");
-                scanf("%d %d", &msg.fila, &msg.columna);
-                if (tablero->tablero[msg.fila][msg.columna] == ' ')
-                {
-                    tablero->tablero[msg.fila][msg.columna] = 'X';
-                    tablero->turno = 2;
-                    if (verificar_ganador(tablero, 'X'))
-                    {
-                        imprimir_tablero(tablero);
-                        printf("¡Jugador X ha ganado!\n");
-                        break;
-                    }
-                    msg.tipo = 2;
-                    msgsnd(qid, &msg, sizeof(msg) - sizeof(long), 0);
-                }
-            }
-            else
-            {
-                msgrcv(qid, &msg, sizeof(msg) - sizeof(long), 1, 0);
-            }
-        }
-        shmdt(tablero);
-        shmctl(shmid, IPC_RMID, NULL);
-        msgctl(qid, IPC_RMID, NULL);
+        jugar(2, msgid, matriz); // Proceso para jugador O
     }
+
+    // Liberar recursos
+    shmdt(matriz);
+    shmctl(shmid, IPC_RMID, NULL);
+    msgctl(msgid, IPC_RMID, NULL);
+
     return 0;
 }
