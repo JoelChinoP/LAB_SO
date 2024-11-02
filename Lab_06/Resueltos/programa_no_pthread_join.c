d545cf2cdc2b5b3b2fa0390975fe322437196734
Joel <jchinop@unsa.edu.pe>
2024-11-02 15:17:49 -0500

    resueltos 1

---
+++ Lab_06/Resueltos/programa_no_pthread_join.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int c = 0;
+
+void *funcion(void *p1)
+{
+    int i = 0;
+    while (i < 300)
+    {
+        c--;
+        printf("Hijo: %d\n", c);
+        i++;
+    }
+    pthread_exit(0);
+}
+
+int main()
+{
+    pthread_t hilo;
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+
+    int error = pthread_create(&hilo, &attr, funcion, NULL);
+    if (error != 0)
+    {
+        perror("error");
+        return (-1);
+    }
+
+    int i = 0;
+    while (i < 300)
+    {
+        c++;
+        printf("Padre: %d\n", c);
+        i++;
+    }
+
+    // Se removió pthread_join(hilo, NULL)
+    return (0);
+}
